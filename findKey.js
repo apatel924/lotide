c316d4d2e03cb07105ea889af37f1b1765599ce5
apatel924 <apatel2@ualberta.ca>
2023-02-10 09:48:09 +0000

    add object and callback w truthy value

---
+++ findKey.js
@@ -0,0 +1,62 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+
+const eqArrays = function(first, second) {
+  //start by checking length
+  if (first.length === second.length) {
+    // now checking if elements are equal to one another
+    for (let i = 0; i < first.length; i++) {
+      if (first[i] !== second[i]) {
+        return false; 
+      }
+    }
+  } else{
+    // return statement if not equal to one another
+    return false
+  }
+  // return statement if elements do not equal same length
+  return true
+}
+
+const assertArraysEqual = function(first, second) {
+  if (eqArrays(first, second)) {
+    console.log(`✅✅✅ Assertion Passed: ${first} === ${second}`)
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${first} !=== ${second}`)
+  }
+}
+
+const findKey = function(object, callback) {
+  //loop throuhg keys in object
+  for (let key in object) {
+  // if callback function returns truthy
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+}
+
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertArraysEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma") // => "noma"
+
